064e73a5844da129edfcd7eca8215396133c953c
jonetxe13 <jonetxe.sasia13@gmail.com>
2026-06-21 21:11:54 +0200

    terminado

--- jechevarria@bender.ehu.eus/src/inrflow/io.h
+++
@@ -1,7 +0,0 @@
-#ifndef _io
-#define _io
-
-long load_mapping_from_file(char *file, long ntasks, long *translation);
-
-long generate_bfs_file();
-#endif
--- jechevarria@bender.ehu.eus/tools/gen_script.sh
+++
@@ -1,1 +0,0 @@
-./gen_workloads 50 50 1 1 64 50 50 sequential random cached 100 1 100
